d20ae0ed410617001e91335b7e1827334a37eb88
anthony <595116717@qq.com>
2017-12-02 17:06:11 +0800

    test image intercept

---
+++ sw.js
@@ -0,0 +1,6 @@
+this.addEventListener('fetch', function (event) {
+    console.log(event)
+    if (/\.jpg$/.test(event.request.url)) {
+        event.respondWidth(fetch('./images/img2.png'))
+    }
+})
