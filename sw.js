99143e4659b14e4bc5d94b17aa9e8964d6725c46
anthony <595116717@qq.com>
2017-12-03 15:03:15 +0800

    添加了静态文件和动态请求的caches

--- sw.js
+++ sw.js
@@ -1,5 +1,38 @@
+var cachName = 'PWA-V1';
+
+// service worker 安装过程中缓存我们已知的文件
+self.addEventListener('install', function (event) {
+    event.waitUntil(
+        caches.open(cachName)
+            .then(function (cache) {
+                cache.addAll([
+                    './main.js',
+                    './images/img2.png'
+                ])
+            })
+    )
+})
+
+//service worker 获取任何请求获取的新资源
 self.addEventListener('fetch', function (event) {
-    if (/\.jpg$/.test(event.request.url)) {
-        event.respondWith(fetch('./images/img2.png'))
-    }
+    event.respondWith(
+        caches.match(event.request, {ignoreSearch: true})
+            .then(function (response) {
+                if (response) {
+                    return response
+                }
+                var requestToCache = event.request.clone()
+                return fetch(requestToCache).then(function (response) {
+                    if (!response || response.status !== 200) {
+                        return response
+                    }
+                    var responseToCache = response.clone()
+                    caches.open(cachName)
+                        .then(function (cache) {
+                            cache.put(requestToCache, responseToCache)
+                        })
+                    return response
+                })
+            })
+    )
 })
